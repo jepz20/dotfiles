a291f114149c3c4e6573a79313105e07877e87d4
Jose Eduardo Perdomo <jepz20@gmail.com>
2018-01-08 13:22:38 -0500

    removed ruby info on powerlevel9k config

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,4 +1,4 @@
 prompt powerlevel9k
 DEFAULT_USER=jepz
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv vcs)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs  nvm time)
