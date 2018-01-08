3c8c354bedecd6246a69502bd52562e9df06e094
Jose Eduardo Perdomo <jepz@MacBook-Pro.local>
2018-01-08 03:04:19 -0500

    use powerlevel9 as default

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,1 +1,4 @@
-prompt skwp
+prompt powerlevel9k
+DEFAULT_USER=jepz
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs  nvm time)
