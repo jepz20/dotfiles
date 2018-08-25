f8e4f02371f1763d02a7b0535c0eb0ce2ccef349
Jose Eduardo Perdomo <jepz20@gmail.com>
2018-08-24 21:44:23 -0400

    user powerlevel9

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,4 +1,4 @@
-prompt powerlevel9k
+prompt pure
 DEFAULT_USER=jepz
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs  nvm time)
